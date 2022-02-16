410ced0b2c015a76326db37ef15bd9eb39a56285
Théo Courdil <courdiltheo@gmail.com>
2022-02-16 16:02:01 +0100

    Header component

--- src/App.js
+++ src/App.js
@@ -1,31 +1,19 @@
-import { BrowserRouter, Link, Route, Routes} from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-import About from './composants/About.js'
-import Home from './composants/Home.js'
-import User from './composants/User.js'
-import Users from './composants/Users.js'
-
+import Header from "./Header";
+import Home from "./Home";
 
 function App() {
   return (
     <BrowserRouter>
-      <header>
-        <nav>
-          <Link to="/">Home</Link>
-          <Link to="/about">À propos</Link>
-          <Link to="/users?q=1">Utilisateurs</Link>
-
-        </nav>
-      </header>
+      <Header />
       <main>
         <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/users/:id" element={<User />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/users" element={<Users />} />
+          <Route path="/" element={<Home />} />
         </Routes>
       </main>
     </BrowserRouter>
-  )
+  );
 }
+
 export default App;
