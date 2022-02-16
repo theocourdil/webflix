1f3a91ef8981c991605e2768fba755b8be1c8e01
Théo Courdil <courdiltheo@gmail.com>
2022-02-16 16:06:58 +0100

    add genre component and data for genre

--- src/composants/Home.js
+++ src/composants/Home.js
@@ -1,8 +1,17 @@
+import Genre from "./Genre";
+
 function Home() {
-    
-    return <div>
-        Webflix
+  return (
+    <div>
+      <h1>
+        Webflix{" "}
+        <span role="img" aria-label="popcorn">
+          🍿
+        </span>
+      </h1>
+      <Genre id="53" />
     </div>
+  );
 }
 
 export default Home;
