613db2368758753918de77568b9f1ca5ef1e655e
Théo Courdil <courdiltheo@gmail.com>
2022-02-17 21:54:11 +0100

    add react query

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
+import { QueryClient, QueryClientProvider } from "react-query";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import Header from "./composants/Header";
 import Home from "./composants/Home";
 import Movie from "./composants/Movie";
 
+const queryClient = new QueryClient();
+
 function App() {
   return (
-    <BrowserRouter>
-      <Header />
-      <main>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/movies/:id" element={<Movie />} />
-        </Routes>
-      </main>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Header />
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/movies/:id" element={<Movie />} />
+          </Routes>
+        </main>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 }
 
