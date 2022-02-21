7b5b07eaadc40d6dbed6e2b06a4c6140ef3f7844
Théo Courdil <courdiltheo@gmail.com>
2022-02-21 09:55:38 +0100

    Use redux for favorites

--- src/composants/Header.js
+++ src/composants/Header.js
@@ -1,12 +1,14 @@
+import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 
 import useStyles from "./composants/Header.style";
 
-function Header({ favorites }) {
+function Header() {
   const classes = useStyles();
+  const favorites = useSelector((state) => state.favorites);
   return (
     <header className={classes.root}>
-      <img className={classes.logo} src="logo32.png" alt="Webflix log" />
+      <img className={classes.logo} src="./logo32.png" alt="Webflix log" />
       <h1 className={classes.title}>Webflix</h1>
       <nav className={classes.nav}>
         <Link className={classes.link} to="/">
